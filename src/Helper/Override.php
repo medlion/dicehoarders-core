f99936dcdf13bb7e9d697e30c02344040a6792a6
Medlion <bvwyngaard@gmail.com>
2019-10-20 21:29:26 +0200

    Added empty helper function ro verify overrides

---
+++ src/Helper/Override.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace App\Helper;
+
+
+class Override
+{
+    public static function verifyOverrideValue (string $key, string $value)
+    {
+        /**
+         * TODO Implement (Hacktoberfest?)
+         *
+         *
+         * This will contain an array of regex for each possible override, that the value can be checked against before adding it
+         */
+    }
+}
