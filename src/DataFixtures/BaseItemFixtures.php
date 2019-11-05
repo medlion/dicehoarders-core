fd16644124347be1bde993cd3809cfa7a6777011
Medlion <bvwyngaard@gmail.com>
2019-11-05 17:47:27 +0200

    Fixture up to weapon range abilities

--- src/DataFixtures/BaseItemFixtures.php
+++ src/DataFixtures/BaseItemFixtures.php
@@ -250,6 +250,8 @@ class BaseItemFixtures extends Fixture implements FixtureGroupInterface
         $needle->setBundleSize(50);
         $ammunitions [] = $needle;
 
+        /** TODO Do all the ammunitions */
+
 
 
         return $ammunitions;
@@ -275,6 +277,50 @@ class BaseItemFixtures extends Fixture implements FixtureGroupInterface
         $ammunitionBullets->setName('Ammunition - Bullets');
         $properties [] = $ammunitionBullets;
 
+        $finesse = new Ability();
+        $finesse->setName('Finesse');
+        $properties [] = $finesse;
+
+        $heavy = new Ability();
+        $heavy->setName('Heavy');
+        $properties [] = $heavy;
+
+        $light = new Ability();
+        $light->setName('Light');
+        $properties [] = $light;
+
+        $loading = new Ability();
+        $loading->setName('Loading');
+        $properties [] = $loading;
+
+        $range515 = new Ability();
+        $range515->setName('Range - 5/15');
+        $properties [] = $range515;
+
+        $range2060 = new Ability();
+        $range2060->setName('Range - 20/60');
+        $properties [] = $range2060;
+
+        $range25100 = new Ability();
+        $range25100->setName('Range - 25/100');
+        $properties [] = $range25100;
+
+        $range30120 = new Ability();
+        $range30120->setName('Range - 30/120');
+        $properties [] = $range30120;
+
+        $range80320 = new Ability();
+        $range80320->setName('Range - 80/320');
+        $properties [] = $range80320;
+
+        $range100400 = new Ability();
+        $range100400->setName('Range - 100/400');
+        $properties [] = $range100400;
+
+        $range150600 = new Ability();
+        $range150600->setName('Range - 150/600');
+        $properties [] = $range150600;
+
         return $properties;
     }
 
@@ -283,6 +329,16 @@ class BaseItemFixtures extends Fixture implements FixtureGroupInterface
         $propertyPart = [];
 
         $ammunitionDescription = 'You can use a weapon that has the ammunition property to make a ranged attack only if you have ammunition to fire from the weapon. Each time you attack with the weapon, you expend one piece of ammunition. Drawing the ammunition from a quiver, case, or other container is part of the attack (you need a free hand to load a one-handed weapon). At the end of the battle, you can recover half your expended ammunition by taking a minute to search the battlefield.'.PHP_EOL.'If you use a weapon that has the ammunition property to make a melee attack, you treat the weapon as an improvised weapon. A sling must be loaded to deal any damage when used in this way';
+        $finesseDescription = 'When making an attack with a finesse weapon, you use your choice of your Strength or Dexterity modifier for the attack and damage rolls. You must use the same modifier for both rolls.';
+        $heavyDescription = 'Small creatures have disadvantage on attack rolls with heavy weapons. A heavy weapon’s size and bulk make it too large for a Small creature to use effectively.';
+        $lightDescription = 'A light weapon is small and easy to handle, making it ideal for use when fighting with two weapons.';
+        $loadingDescription = 'Because of the time required to load this weapon, you can fire only one piece of ammunition from it when you use an action, bonus action, or reaction to fire it, regardless of the number of attacks you can normally make.';
+        $rangeDescription = 'Because of the time required to load this weapon, you can fire only one piece of ammunition from it when you use an action, bonus action, or reaction to fire it, regardless of the number of attacks you can normally make.';
+        $reachDescription = 'This weapon adds 5 feet to your reach when you attack with it, as well as when determining your reach for opportunity attacks with it.';
+        $specialDescription = 'A weapon with the special property has unusual rules governing its use, explained in the weapon’s description (see “Special Weapons” later in this section).';
+        $thrownDescription = 'If a weapon has the thrown property, you can throw the weapon to make a ranged attack. If the weapon is a melee weapon, you use the same ability modifier for that attack roll and damage roll that you would use for a melee attack with the weapon. For example, if you throw a handaxe, you use your Strength, but if you throw a dagger, you can use either your Strength or your Dexterity, since the dagger has the finesse property.';
+        $twoHandedDescription = 'This weapon requires two hands when you attack with it.';
+        $versatileDescription = 'This weapon can be used with one or two hands. A damage value in parentheses appears with the property—the damage when the weapon is used with two hands to make a melee attack.';
 
         $abilityAmmunitionArrows = $abilityRepository->findOneBy(['name' => 'Ammunition - Arrows']);
         $ammunitionGenericArrow = new AbilityGeneric();
@@ -327,6 +383,118 @@ class BaseItemFixtures extends Fixture implements FixtureGroupInterface
         $abilityOverrideBullet->setOverrideKey('ammunition');
         $abilityOverrideBullet->setValue('Sling Bullet');
         $propertyPart [] = $abilityOverrideBullet;
+
+        /**
+         * TODO Finesse should probably a custom function return type of method. But since that isn't implemented in the first place, it is not. And that is gonna be a doozy
+         */
+        $abilityFinesse = $abilityRepository->findOneBy(['name' => 'Finesse']);
+        $finesseGeneric = new AbilityGeneric();
+        $finesseGeneric->setAbility($abilityFinesse);
+        $finesseGeneric->setDescription($finesseDescription);
+        $propertyPart [] = $finesseGeneric;
+
+        $abilityHeavy = $abilityRepository->findOneBy(['name' => 'Heavy']);
+        $heavyGeneric = new AbilityGeneric();
+        $heavyGeneric->setAbility($abilityHeavy);
+        $heavyGeneric->setDescription($heavyDescription);
+        $propertyPart [] = $heavyGeneric;
+
+        /** TODO Light may also be an override? Then again maybe not. It's existence alone can be a check */
+        $abilityLight = $abilityRepository->findOneBy(['name' => 'Light']);
+        $lightGeneric = new AbilityGeneric();
+        $lightGeneric->setAbility($abilityLight);
+        $lightGeneric->setDescription($lightDescription);
+        $propertyPart [] = $lightGeneric;
+
+        $loadingAbility = $abilityRepository->findOneBy(['name' => 'Loading']);
+        $loadingGeneric = new AbilityGeneric();
+        $loadingGeneric->setAbility($loadingAbility);
+        $loadingGeneric->setDescription($loadingDescription);
+        $propertyPart [] = $loadingGeneric;
+
+        $abilityRange515 = $abilityRepository->findOneBy(['name' => 'Range - 5/15']);
+        $abilityOverrideNormal515 = new AbilityOverride();
+        $abilityOverrideNormal515->setAbility($abilityRange515);
+        $abilityOverrideNormal515->setOverrideKey('range_normal');
+        $abilityOverrideNormal515->setValue('5');
+        $propertyPart [] = $abilityOverrideNormal515;
+        $abilityOverrideDisadvantage515 = new AbilityOverride();
+        $abilityOverrideDisadvantage515->setAbility($abilityRange515);
+        $abilityOverrideDisadvantage515->setOverrideKey('range_disadvantage');
+        $abilityOverrideDisadvantage515->setValue('15');
+        $propertyPart [] = $abilityOverrideDisadvantage515;
+
+        $abilityRange2060 = $abilityRepository->findOneBy(['name' => 'Range - 20/60']);
+        $abilityOverrideNormal2060 = new AbilityOverride();
+        $abilityOverrideNormal2060->setAbility($abilityRange2060);
+        $abilityOverrideNormal2060->setOverrideKey('range_normal');
+        $abilityOverrideNormal2060->setValue('20');
+        $propertyPart [] = $abilityOverrideNormal2060;
+        $abilityOverrideDisadvantage2060 = new AbilityOverride();
+        $abilityOverrideDisadvantage2060->setAbility($abilityRange2060);
+        $abilityOverrideDisadvantage2060->setOverrideKey('range_disadvantage');
+        $abilityOverrideDisadvantage2060->setValue('60');
+        $propertyPart [] = $abilityOverrideDisadvantage2060;
+
+        $abilityRange25100 = $abilityRepository->findOneBy(['name' => 'Range - 25/100']);
+        $abilityOverrideNormal25100 = new AbilityOverride();
+        $abilityOverrideNormal25100->setAbility($abilityRange25100);
+        $abilityOverrideNormal25100->setOverrideKey('range_normal');
+        $abilityOverrideNormal25100->setValue('25');
+        $propertyPart [] = $abilityOverrideNormal25100;
+        $abilityOverrideDisadvantage25100 = new AbilityOverride();
+        $abilityOverrideDisadvantage25100->setAbility($abilityRange25100);
+        $abilityOverrideDisadvantage25100->setOverrideKey('range_disadvantage');
+        $abilityOverrideDisadvantage25100->setValue('100');
+        $propertyPart [] = $abilityOverrideDisadvantage25100;
+
+        $abilityRange30120 = $abilityRepository->findOneBy(['name' => 'Range - 30/120']);
+        $abilityOverrideNormal30120 = new AbilityOverride();
+        $abilityOverrideNormal30120->setAbility($abilityRange30120);
+        $abilityOverrideNormal30120->setOverrideKey('range_normal');
+        $abilityOverrideNormal30120->setValue('30');
+        $propertyPart [] = $abilityOverrideNormal30120;
+        $abilityOverrideDisadvantage30120 = new AbilityOverride();
+        $abilityOverrideDisadvantage30120->setAbility($abilityRange30120);
+        $abilityOverrideDisadvantage30120->setOverrideKey('range_disadvantage');
+        $abilityOverrideDisadvantage30120->setValue('120');
+        $propertyPart [] = $abilityOverrideDisadvantage30120;
+
+        $abilityRange80320 = $abilityRepository->findOneBy(['name' => 'Range - 80/320']);
+        $abilityOverrideNormal80320 = new AbilityOverride();
+        $abilityOverrideNormal80320->setAbility($abilityRange80320);
+        $abilityOverrideNormal80320->setOverrideKey('range_normal');
+        $abilityOverrideNormal80320->setValue('80');
+        $propertyPart [] = $abilityOverrideNormal80320;
+        $abilityOverrideDisadvantage80320 = new AbilityOverride();
+        $abilityOverrideDisadvantage80320->setAbility($abilityRange80320);
+        $abilityOverrideDisadvantage80320->setOverrideKey('range_disadvantage');
+        $abilityOverrideDisadvantage80320->setValue('320');
+        $propertyPart [] = $abilityOverrideDisadvantage80320;
+
+        $abilityRange100400 = $abilityRepository->findOneBy(['name' => 'Range - 100/400']);
+        $abilityOverrideNormal100400 = new AbilityOverride();
+        $abilityOverrideNormal100400->setAbility($abilityRange100400);
+        $abilityOverrideNormal100400->setOverrideKey('range_normal');
+        $abilityOverrideNormal100400->setValue('100');
+        $propertyPart [] = $abilityOverrideNormal100400;
+        $abilityOverrideDisadvantage100400 = new AbilityOverride();
+        $abilityOverrideDisadvantage100400->setAbility($abilityRange100400);
+        $abilityOverrideDisadvantage100400->setOverrideKey('range_disadvantage');
+        $abilityOverrideDisadvantage100400->setValue('400');
+        $propertyPart [] = $abilityOverrideDisadvantage100400;
+
+        $abilityRange150600 = $abilityRepository->findOneBy(['name' => 'Range - 150/600']);
+        $abilityOverrideNormal150600 = new AbilityOverride();
+        $abilityOverrideNormal150600->setAbility($abilityRange150600);
+        $abilityOverrideNormal150600->setOverrideKey('range_normal');
+        $abilityOverrideNormal150600->setValue('150');
+        $propertyPart [] = $abilityOverrideNormal150600;
+        $abilityOverrideDisadvantage150600 = new AbilityOverride();
+        $abilityOverrideDisadvantage150600->setAbility($abilityRange150600);
+        $abilityOverrideDisadvantage150600->setOverrideKey('range_disadvantage');
+        $abilityOverrideDisadvantage150600->setValue('600');
+        $propertyPart [] = $abilityOverrideDisadvantage150600;
 
         return $propertyPart;
     }
